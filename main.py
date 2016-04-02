0955a853559135d9bec5293363e693b47493f6da
frknnay <furkan-ay@outlook.com>
2016-04-03 01:04:09 +0300

    number imput improved

--- main.py
+++ main.py
@@ -12,12 +12,19 @@ def print_board(board):
         print(' '.join(row))
 
 def fill_the_board():
-    message = "Enter a number (X). Put '?' if you don't know the number: "
+    message = "Enter a number (1-9). Put '?' if you don't know the number: "
     for i in range(len(board)):
         for x in range(len(board[i])):
             board[i][x] = 'X'
             print_board(board)
-            board[i][x] = input(message)
+            while True:
+                number = input(message)
+                if number.isdigit() and 1 <= int(number) <= 9:
+                    break
+                else:
+                    print("Number should be between 1 and 9")
+
+            board[i][x] = number
 
 
 print_board(board)
